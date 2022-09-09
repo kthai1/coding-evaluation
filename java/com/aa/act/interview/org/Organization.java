8e307fd8b0aca4d7ac0617ac4fef9d239139615b
kthai1 <kevinthai594@yahoo.com>
2022-09-09 14:53:29 -0700

    coding evaluation sumbission

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -1,11 +1,13 @@
 package com.aa.act.interview.org;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 public abstract class Organization {
 
 	private Position root;
-	
+	private int employeeID = 1;
+
 	public Organization() {
 		root = createOrganization();
 	}
@@ -21,7 +23,47 @@ public abstract class Organization {
 	 */
 	public Optional<Position> hire(Name person, String title) {
 		//your code here
-		return Optional.empty();
+		return recursiveMethod(root, person, title);
+	}
+
+	public Optional<Position> recursiveMethod(Position position, Name person, String title) {
+
+		//variables
+		Optional<Position> newPos = Optional.empty();
+
+		//2 conditions
+		//if title exists/matches the current position, return it
+		if (position.getTitle() == title){
+
+			//create employee
+			Employee newEmp = new Employee(employeeID, person);
+
+			//checks if employee exists via their title, skips them
+			if (!position.isFilled()){
+
+				//Increment Employee's unique ID
+				employeeID = employeeID + 1;
+
+				//hire employee, assign the newly created employee to the position
+				position.setEmployee(Optional.of(newEmp));
+				newPos = Optional.of(position);
+			}
+		}
+		//else if doesn't check if direct report if title exists
+		if (position.getDirectReports() != null){
+
+			//loop through the tree of direct reports for the title
+//			for (int i = 0; i < root.getDirectReports().size(); i++){
+//				System.out.println(root);
+//			}
+			//loop through the current position's direct reports using a for each loop
+			for (Position currPos : position.getDirectReports()){
+
+				//recursive call to loop through the previous position's direct reports in the for loop
+				newPos = recursiveMethod(currPos, person, title);
+			}
+		}
+		return newPos;
 	}
 
 	@Override
